10ea0a97f923fdce4f3c09cc9171fc92863aed82
Maricar Finn <maricar.finn@gmail.com>
2020-06-15 16:58:45 +0200

    Finished course

---
+++ BullsEye/BullsEye/AboutAuthourViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AboutAuthourViewController.swift
+//  BullsEye
+//
+//  Created by Maricar Finn on 15/06/2020.
+//  Copyright © 2020 Maricar Finn. All rights reserved.
+//
+
+import UIKit
+
+class AboutAuthourViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
